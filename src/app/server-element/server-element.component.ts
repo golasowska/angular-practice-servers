72771991bc53dad620ef3dd52b2c88b455160692
golasowska <annagolasowska@gmail.com>
2018-02-21 13:58:14 +0100

    lifecycle

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,11 @@
-import {Component, OnInit, Input, ViewEncapsulation} from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewEncapsulation,
+  OnChanges,
+  SimpleChanges
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,12 +13,20 @@ import {Component, OnInit, Input, ViewEncapsulation} from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated   // <--default None, Native
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges {
   @Input('srvElement') element: {type: string, name: string, content: string };
 
-  constructor() { }
+  constructor() {
+    console.log('Constructor called!')
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('OnChanges called!');
+    console.log(changes)
+  }
 
   ngOnInit() {
+    console.log('OnInit called!')
   }
 
 }
